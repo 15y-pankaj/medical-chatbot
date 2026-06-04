c0cecf498b8f088be946e12182831fd9438dc14e
15y-pankaj <15y.pankaj@gmail.com>
2026-06-04 13:18:15 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Pankaj Yadav' ,
+    version='0.0.1',
+    author='Pankaj Yadav',
+    author_email='15y.pankaj@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
